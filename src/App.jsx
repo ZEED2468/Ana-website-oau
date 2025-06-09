776528a14e4ca77663064584af259d09a84b159d
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-06-09 11:25:28 +0100

    still setting up and working on the navbar

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className="text-3xl font-bold underline text-red-500 bg-blue-500">
+        Hello world!
+      </h1>
+    </>
+  )
+}
+
+
+export default App;
