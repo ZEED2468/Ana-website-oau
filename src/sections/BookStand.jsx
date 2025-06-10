6fdac7e286ce702b03d4cbf8a904d1bfbf156ad4
baruwa abdul-azeez mayowa <mayorkunabdulazeez01@gmail.com>
2025-06-10 13:59:24 +0100

    still working on the bookStand component

--- src/sections/BookStand.jsx
+++ src/sections/BookStand.jsx
@@ -1,34 +1,36 @@
-import React from 'react'
+import React from "react";
 
 const BookStand = () => {
-    return (
-        <div className="flex justify-between items-center rounded-[1.5rem] border border-[#B9BBC6] bg-[#C2EEF2]">
-            <div className="flex flex-col gap-5 flex-start">
-                <h1 className="text-3xl font-bold special-font lg:w-[22.8125rem] text-[#1E1F24]">
-                    Our Sponsors & Partners
-                </h1>
-                <p className="text-[#62636C] lg:w-[26.75rem]">
-                    8 incredible guest artists, poets, and creators will join us to speak, perform, and inspire.
-                </p>
-                <a
-                    href="https://anaoau.org/book-festival"
-                    target="_blank"
-                    rel="noopener noreferrer"
-                >
-                    <button className="bg-[#3D63DD] text-white px-6 py-2 border-2 border-[#000000] rounded my-4 transition duration-300 shadow-[8px_8px_0px_0px_rgba(0,0,0,1)]">
-                    Partner With Us
-                    </button>
-                </a>
-            </div>
-            <div>
-                <img
-                    src="/shop1.svg"
-                    alt="Book Stand"
-                    className="block w-full h-auto object-cover lg:w-[32.5625rem] h-[25.3125rem]"
-                />
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="flex flex-col lg:flex-row justify-between items-center rounded-[1.5rem] border border-[#B9BBC6] bg-[#C2EEF2] m-5 p-0 lg:p-[6.5rem_7.5rem]">
+      <div className="flex flex-col gap-5 flex-start w-full px-5 pt-14 pb-4 lg:w-auto lg:px-0 lg:py-0 text-center lg:text-left">
+        <h1 className="text-3xl font-bold special-font lg:max-w-[22.8125rem] text-[#1E1F24]">
+          Book a Stand
+        </h1>
+        <p className="text-[#62636C] lg:max-w-[26.75rem]">
+          Have something to sell or showcase? Join the marketplace of creativity.
+        </p>
+        <a
+          href="https://anaoau.org/book-festival"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="mt-2 mb-8 lg:mb-0"
+        >
+          <button className="bg-[#2D93D0] lg:bg-[#3D63DD] text-white p-[0.5rem_1.5rem] lg:p-[0.5rem_1.5rem] border-2 border-[#000000] rounded my-4 transition duration-300 shadow-[8px_8px_0px_0px_rgba(0,0,0,1)]">
+            <span className="lg:hidden">Book a Stand</span>
+            <span className="hidden lg:inline">Partner With Us</span>
+          </button>
+        </a>
+      </div>
+      <div className="w-full lg:w-auto px-5 lg:px-0 lg:pr-5">
+        <img
+          src="/shop1.svg"
+          alt="Book Stand Illustration"
+          className="w-full h-auto object-contain max-w-[32.5625rem] max-h-[25.3125rem]"
+        />
+      </div>
+    </div>
+  );
+};
 
-export default BookStand
+export default BookStand;
